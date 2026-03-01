19353a49f608cd6cdde77141b16b456ce768587b
DC <easetreychen@gmail.com>
2026-03-01 17:23:13 +0800

    refactor: improve formatting and consistency in practice-reactive.js, enhancing readability of reactive system functions

--- src/vue3/practice-reactive.js
+++ src/vue3/practice-reactive.js
@@ -1,56 +1,65 @@
 let activeEffect = null
 
 function effect(fn) {
-    activeEffect = fn
-    fn();
-    activeEffect = null
+  activeEffect = fn
+  fn();
+  activeEffect = null
 }
 
 
-function reactive(obj){
-    return new Proxy(obj,{
-        get(target,key){
-            console.log("get")
-            return target[key]
-        },
-        set(target,key,value){
-            console.log(set)
-             target[key] = value
-             return true
+function reactive(obj) {
+  return new Proxy(obj, {
+    get(target, key) {
+      console.log("get")
+      return target[key]
+    },
+    set(target, key, value) {
+      console.log(set)
+      target[key] = value
+      return true
 
-        },
-    })
+    },
+  })
 }
 
 const bucket = new WeakMap()
 
-function track(target,key){
-  if(!activeEffect) return;
+function track(target, key) {
+  if (!activeEffect) return;
 
   let depsMap = bucket.get(target)
-  if(!depsMap){
+  if (!depsMap) {
     depsMap = new Map()
-    bucket.set(target,depsMap)
+    bucket.set(target, depsMap)
   }
 
   let dep = depsMap.get(key)
-  if(!dep){
+  if (!dep) {
     dep = new Set()
-    depsMap.set(key,dep)
+    depsMap.set(key, dep)
   }
-   dep.add(activeEffect)
+  dep.add(activeEffect)
 }
 
-function trigger(target,key,value){
+function trigger(target, key, value) {
+
 
   const depsMap = bucket.get(target)
 
-  if(!depsMap) return;
+  if (!depsMap) return;
 
   const dep = depsMap.get(key)
-  if(!dep) return;
+  if (!dep) return;
+
+  const effectToRun = new Set();
 
   dep.forEach(element => {
+    if (element !== activeEffect) {
+      effectToRun.add(element)
+    }
+  });
+
+  effectToRun.forEach(element => {
     element.run()
   });
 
